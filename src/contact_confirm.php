b7aa0f61af24bdb4e5af25de1dac6148dc1a5532
alexvnilsson <alexvnilsson@outlook.com>
2020-11-16 11:18:48 +0100

    Add contact form and confirm pages

---
+++ src/contact_confirm.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Kontaktbekräftelse
+ *
+ * Template file for handling form submissions.
+ *
+ * @package {{ template.name }}
+ * @version {{ templateVersion }}
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+    <p><?php echo the_content(); ?></p>
+</div>
+
+<?php get_footer(); ?>

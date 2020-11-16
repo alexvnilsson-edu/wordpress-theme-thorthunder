f8fa4d68e4dbe9ab3c62dd8f23da72602a6aa56b
alexvnilsson <alexvnilsson@outlook.com>
2020-11-16 10:57:43 +0100

    Update header.php to insert SEO-meta tags

--- src/header.php
+++ src/header.php
@@ -11,27 +11,37 @@ require_once(THEME_PATH . "/includes/Module/Header.php");
 use ThorThunder\WordPressTheme\Core\Menu;
 use ThorThunder\WordPressTheme\Module\Header;
 
+$excerpt = null;
+if (is_single()) {
+    $excerpt = get_the_excerpt();
+}
+
 ?>
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
 <head>
-    <?php if (is_single() || is_page() || is_home()) { ?>
+    <?php if (is_single() || is_page() || is_home()): ?>
     <meta name="googlebot" content="index,noarchive,follow,noodp" />
     <meta name="robots" content="all,index,follow" />
     <meta name="msnbot" content="all,index,follow" />
-    <?php } else { ?>
+    <?php else: ?>
     <meta name="googlebot" content="noindex,noarchive,follow,noodp" />
     <meta name="robots" content="noindex,follow" />
     <meta name="msnbot" content="noindex,follow" />
-    <?php }?>
+    <?php endif; ?>
+
+    <?php if ($excerpt): ?>
+    <meta name="description" content="<?php echo $excerpt; ?>" />
+    <?php else: ?>
+    <meta name="description" content="<?php bloginfo('description') ?>">
+    <?php endif; ?>
 
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('name'); wp_title(); ?></title>
-    <meta name="description" content="<?php bloginfo('description') ?>">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php if (is_singular() && pings_open(get_queried_object())): ?>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
@@ -49,9 +59,11 @@ use ThorThunder\WordPressTheme\Module\Header;
             <?php if (has_custom_logo()): ?>
             <img src="<?php echo $logo[0] ?>" width="<?php echo $logo[1] ?>" height="<?php echo $logo[2] ?>"
                 alt="Logotyp" />
+            <?php else: ?>
+            <span class="name"><?php bloginfo('name'); ?></span>
+
             <?php endif ?>
 
-            <span class="name"><?php bloginfo('name'); ?></span>
         </a>
 
         <a href="#" class="menu-expander">
